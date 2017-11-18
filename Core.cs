e4c13ec8df5038b53ceab8fc19642afd0f57cc52
Ryan French <Frenchi@Frenchi.net>
2017-11-17 19:38:56 -0500

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronSockets;
+
+namespace CoolMaster_NET_Controller {
+
+    public static class Core {
+
+        //===================// Members //===================//
+
+        internal static string ipa;
+        internal static int    port;
+        internal static bool   okToConnect;
+        internal static bool   waitForTx;
+
+        internal static Dictionary<string, Zone> Zones;
+        internal static List<string> MessageQueue;
+
+        internal static CTimer pollTimer;
+        internal static CTimer reconnectTimer;
+
+        internal static TCPClient client;
+
+        public delegate void ConnectionStatus(ushort status);
+        public static ConnectionStatus ConnectionStatusEvent { get; set; }
+
+
+        //===================// Constructor //===================//
+
+        static Core() {
+
+            Zones        = new Dictionary<string, Zone>();
+            MessageQueue = new List<string>();
+
+        }
+
+        //===================// Methods //===================//
+
+        //-------------------------------------//
+        // FUNCTION: TCPClientSettings
+        // Description: ...
+        //-------------------------------------//
+
+        public static void TCPClientSettings(string _ip, ushort _prt) {
+
+            ipa  = _ip;
+            port = _prt;
+            okToConnect = true;
+
+            client = new TCPClient(ipa, port, 10240);
+            client.SocketStatusChange += new TCPClientSocketStatusChangeEventHandler(clientSocketChange);
+            DeviceConnect();
+            reconnectTimer = new CTimer(reconnectTimerHandler, 15000);
+
+        }
+
+        //-------------------------------------//
+        // FUNCTION: RegisterZone
+        // Description: ...
+        //-------------------------------------//
+
+        public static ushort RegisterZone (string _uid, Zone _zone) {
+
+            if (!Zones.ContainsKey(_uid)) {
+                Zones.Add(_uid, _zone);
+                return 1;
+            } else {
+                return 0;
+            }
+
+        }
+
+        //-------------------------------------//
+        // FUNCTION: DeviceConnect
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void DeviceConnect () {
+
+            try {
+
+                client.ConnectToServerAsync(clientConnect);
+
+            } catch (Exception _er) {
+
+                ErrorLog.Error("Error connecting to CoolMaster device at address {0}: {1}", ipa, _er);
+
+            }
+
+        }
+
+        //-------------------------------------//
+        // FUNCTION: ParseData
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void ParseFeedback(string _data) {
+
+            string[] lines = _data.Split('\x0D');
+            Zone _zn;
+
+            string uid;
+
+            for (int i = 0; i < lines.Length; i++) {
+
+                if (lines[i] == "\x0A")
+                    continue;
+
+                if(lines[i][0] == '\x0A')
+                    lines[i] = lines[i].Substring(1);
+
+                if(lines[i].IndexOf("L") >= 0) {
+
+                    uid     = lines[i].Substring(0, 6);
+
+                    if (Zones.ContainsKey(uid)) {
+                        _zn       = Zones[uid];
+                        _zn.CorF  = lines[i][15] == 'C' ? "C" : "F";
+                        _zn.OnOff = lines[i].Substring(7, 3).Trim();
+
+                        if (_zn.CorF == "F") {
+                            if (!_zn.lockSetpoint) {
+                                _zn.SetpointRaw = lines[i].Substring(11, 5).TrimStart('0');
+                                _zn.Setpoint    = float.Parse(_zn.SetpointRaw);
+                            }
+                            _zn.TempRaw    = lines[i].Substring(18, 5).TrimStart('0');
+                            _zn.FanSpeed   = lines[i].Substring(25, 4).Trim();
+                            _zn.SystemMode = lines[i].Substring(30, 4).Trim();
+                            _zn.Demand     = lines[i].Substring(42, 1) == "1" ? true : false;
+                        } else if (_zn.CorF == "C") {
+                            if (!_zn.lockSetpoint) {
+                                _zn.SetpointRaw = lines[i].Substring(11, 4).TrimStart('0');
+                                _zn.Setpoint    = float.Parse(_zn.SetpointRaw);
+                            }
+                            _zn.TempRaw    = lines[i].Substring(17, 4).TrimStart('0');
+                            _zn.FanSpeed   = lines[i].Substring(23, 4).Trim();
+                            _zn.SystemMode = lines[i].Substring(38, 4).Trim();
+                            _zn.Demand     = lines[i].Substring(40, 1) == "1" ? true : false;
+                        }
+
+                        _zn.Temp = float.Parse(_zn.TempRaw);
+
+                        _zn.Update();
+                    }
+
+                }
+            }
+
+        }
+
+        //-------------------------------------//
+        // FUNCTION: QueueCommand
+        // Description: ...
+        //-------------------------------------//
+
+        public static void QueueCommand(string _cmd) {
+
+            // Ignore new commands if controller is disconnected
+            if (client.ClientStatus != SocketStatus.SOCKET_STATUS_CONNECTED)
+                return;
+
+            string command = _cmd + "\x0D\x0A";
+
+            if (waitForTx) {
+                MessageQueue.Add(command);
+            } else {
+                waitForTx = true;
+                client.SendDataAsync(Encoding.ASCII.GetBytes(command), command.Length, clientDataTX);
+            }
+
+        }
+
+        //===================// Event Handlers //===================//
+
+        //-------------------------------------//
+        // EVENTHANDLER: clientSocketChange
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void clientSocketChange(TCPClient _cli, SocketStatus _status) {
+
+            if (_status != SocketStatus.SOCKET_STATUS_CONNECTED && okToConnect) {
+
+                ConnectionStatusEvent(0);
+                pollTimer.Stop();
+                DeviceConnect();
+                reconnectTimer = new CTimer(reconnectTimerHandler, 15000);
+
+            } else if (_status == SocketStatus.SOCKET_STATUS_CONNECTED) {
+
+                ConnectionStatusEvent(1);
+
+            }
+
+        }
+
+        //-------------------------------------//
+        // EVENTHANDLER: clientDataRX
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void clientDataRX(TCPClient _cli, int _bytes) {
+
+            string data = Encoding.ASCII.GetString(_cli.IncomingDataBuffer, 0, _bytes);
+
+            if (data != ">" && data != "")
+                ParseFeedback(data);
+
+            client.ReceiveDataAsync(clientDataRX);
+
+        }
+
+        //-------------------------------------//
+        // EVENTHANDLER: clientDataTX
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void clientDataTX(TCPClient _cli, int _bytes) {
+
+            if (MessageQueue.Count > 0) {
+                client.SendDataAsync(Encoding.ASCII.GetBytes(MessageQueue[0]), MessageQueue[0].Length, clientDataTX);
+                MessageQueue.RemoveAt(0);
+            } else {
+                waitForTx = false;
+            }
+
+        }
+
+        //-------------------------------------//
+        // EVENTHANDLER: clientConnect
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void clientConnect (TCPClient _cli) {
+
+            client.ReceiveDataAsync(clientDataRX);
+
+            // Start poll timer
+            pollTimer = new CTimer(pollTimerHandler, 2000);
+
+        }
+
+        //-------------------------------------//
+        // EVENTHANDLER: pollTimerHandler
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void pollTimerHandler (object o) {
+            
+            // Send ls2 command
+            if(client.ClientStatus == SocketStatus.SOCKET_STATUS_CONNECTED) {
+                byte[] data = new byte[5];
+                client.SendDataAsync(Encoding.ASCII.GetBytes("ls2\x0D\x0A"), 5, clientDataTX);
+
+                // Reset timer
+                pollTimer = new CTimer(pollTimerHandler, 2000);
+            }
+
+        }
+
+        //-------------------------------------//
+        // EVENTHANDLER: reconnectTimerHandler
+        // Description: ...
+        //-------------------------------------//
+
+        internal static void reconnectTimerHandler(object o) {
+
+            if (client.ClientStatus != SocketStatus.SOCKET_STATUS_CONNECTED) {
+                DeviceConnect();
+                // Reset timer
+                reconnectTimer = new CTimer(reconnectTimerHandler, 15000);
+            }
+
+        }
+
+    } // End Core class
+
+    public delegate void DelegateUshortString(ushort value1, SimplSharpString value2);
+
+}
---
+++ Zone.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace CoolMaster_NET_Controller {
+
+    public class Zone {
+
+        //===================// Members //===================//
+
+        public string Name;
+
+        public string UID;
+        public string OnOff;
+        public string CorF;
+
+        public string SetpointRaw;
+        public float  Setpoint;
+
+        public string TempRaw;
+        public float  Temp;
+
+        public string FanSpeed;
+        public string SystemMode;
+
+        public bool Demand;
+
+        public bool lockSetpoint;
+        internal CTimer setpointLockout;
+
+        public delegate void ZoneFeedback(ushort OnOff, 
+                                          ushort SetPtAna,
+                                          SimplSharpString SetPtStr, 
+                                          ushort TempAna,
+                                          SimplSharpString TempStr,
+                                          SimplSharpString FanSpd,
+                                          SimplSharpString SysMode,
+                                          ushort demand);
+        public ZoneFeedback ZoneFeedbackEvent { get; set; }
+
+        public delegate void SetpointFeedback(ushort SetPtAna, SimplSharpString SetPtStr);
+        public SetpointFeedback SetpointFeedbackEvent { get; set; }
+
+        //===================// Constructor //===================//
+
+        public Zone() {
+
+            Name = "";
+            UID = "";
+            OnOff = "";
+            CorF = "";
+            SetpointRaw = "";
+            TempRaw = "";
+            FanSpeed = "";
+            SystemMode = "";
+
+            Setpoint = 0f;
+            Temp = 0f;
+
+            Demand = false;
+
+            lockSetpoint = false;
+
+        }
+
+        //===================// Methods //===================//
+
+        //-------------------------------------//
+        // FUNCTION: SetUID
+        // Description: ...
+        //-------------------------------------//
+
+        public void Configure(string _name, string _uid) {
+
+            Name = _name;
+            UID  = _uid;
+
+        }
+
+        //-------------------------------------//
+        // FUNCTION: Update
+        // Description: Called by Core after data for this zone has been
+        //              read from the controller
+        //-------------------------------------//
+
+        public void Update () {
+
+            // Send values to S+
+            ZoneFeedbackEvent(
+                (ushort)(OnOff == "ON" ? 1 : 0),
+                (ushort)(Setpoint * 10),
+                SetpointRaw,
+                (ushort)(Temp * 10),
+                TempRaw,
+                FanSpeed,
+                SystemMode,
+                (ushort)(Demand ? 1 : 0)
+            );
+
+        }
+
+        //-------------------------------------//
+        // FUNCTION: SetOnOff
+        // Description: ...
+        //-------------------------------------//
+
+        public void SetOnOff(ushort _state) {
+
+            Core.QueueCommand(String.Format("{0} {1}", _state == 1 ? "on" : "off", UID));
+
+        }
+
+        public void SetSysMode(string _state) {
+
+            Core.QueueCommand(String.Format("{0} {1}", _state, UID));
+
+        }
+
+        public void SetFanSpeed(string _state) {
+
+            Core.QueueCommand(String.Format("fspeed {0} {1}", UID, _state));
+
+        }
+
+        public void SetpointUpDown(ushort _dir) {
+
+            // Ignore command if controller hasn't finished poll yet
+            if (Setpoint == 0f) 
+                return;
+
+            // Set flag to lock out feedback from controller
+            if (!lockSetpoint) {
+                lockSetpoint = true;
+                setpointLockout = new CTimer(SetpointLockoutExpired, 5000);
+            } else {
+                setpointLockout.Stop();
+                setpointLockout.Reset(5000);
+            }
+
+            // Update local variable
+            Setpoint += _dir == 1 ? 1.0f : -1.0f;
+            SetpointRaw = Setpoint.ToString();
+
+            SetpointFeedbackEvent((ushort)(Setpoint * 10), SetpointRaw); 
+
+        }
+
+        public void SetpointDirect(ushort _val) {
+            Setpoint = ((float)_val)/10;
+            SetpointRaw = Setpoint.ToString();
+            SetpointFeedbackEvent((ushort)(Setpoint * 10), SetpointRaw);
+            Core.QueueCommand(String.Format("temp {0} {1}", UID, Setpoint));
+        }
+
+        //===================// Event Handlers //===================//
+
+        internal void SetpointLockoutExpired(object o) {
+
+            // Release setpoint lock
+            lockSetpoint = false;
+
+            // Send new setpoint value
+            Core.QueueCommand(String.Format("temp {0} {1}", UID, Setpoint));
+
+        }
+
+    } // End Zone class
+
+}
